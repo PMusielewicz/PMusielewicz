250e96f5d598dafb03ab384c69847c0acdd2cab9
PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
2023-06-22 20:26:32 +0200

    Przykładowy opis zadania 2

--- cpp.cpp
+++ cpp.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
- 
+
 int main()
 {
     cout << "Wiosna" << endl;
     cout << "Lato";
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Wiosna\n";
+    cout << "La\nto\n";
     return 0;
 }

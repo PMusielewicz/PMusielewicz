b1a61e44c3ef738d84a41c9cbbd0c4bdd7a0faa5
PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
2023-06-22 21:05:58 +0200

    kolejny commit

--- cpp.cpp
+++ cpp.cpp
@@ -1,33 +1,41 @@
-#include <iostream>
-using namespace std;
+[33mcommit f96004cc43e2650475276c4dea8a8ec1f970c70e[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
+Date:   Thu Jun 22 20:50:16 2023 +0200
 
-int main()
-{
-    cout << "Wiosna" << endl;
-    cout << "Lato";
-    return 0;
-}
-#include <iostream>
-using namespace std;
+    do usunięcia
 
-int main()
-{
-    cout << "Wiosna\n";
-    cout << "La\nto\n";
-    return 0;
-}
+[33mcommit 30123cb11e8ee5a5b142397c59f7daf5d23cec7b[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
+Date:   Thu Jun 22 20:28:41 2023 +0200
 
-int main()
-{
-    int l=100;
-    cout << "l= " << l << endl;
-    cout << "2*l+1= " << 2*l+1 << endl;
- 
-    cout << "Podaj liczbe: ";
-    cin >> l;
-    cout << "Wpisales " << l << endl;
- 
-    return 0;
-}
+    Przykładowy opis zadania 3
 
-dasdasdasdasdsadasd
+[33mcommit 250e96f5d598dafb03ab384c69847c0acdd2cab9[m
+Author: PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
+Date:   Thu Jun 22 20:26:32 2023 +0200
+
+    Przykładowy opis zadania 2
+
+[33mcommit a4f8cbf8b1d4f16cd940f952796ed0031a5933b7[m
+Author: PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
+Date:   Thu Jun 22 20:24:17 2023 +0200
+
+    Przykładowy opis zadania
+
+[33mcommit 455e3e8514ee6a22d0b88f228281ae66ab855678[m
+Author: PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
+Date:   Thu Jun 22 20:14:11 2023 +0200
+
+    Update README.md
+
+[33mcommit fe13a7a7dd5ca0eef02a9acb43df8cebbc6db71a[m
+Author: PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
+Date:   Thu Jun 22 20:08:19 2023 +0200
+
+     README.md
+
+[33mcommit fb5fc57edb5ee29fc1edc55d12a571a69fa75fb1[m
+Author: PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
+Date:   Thu Jun 22 20:07:20 2023 +0200
+
+    Initial commit

30123cb11e8ee5a5b142397c59f7daf5d23cec7b
PMusielewicz <137438501+PMusielewicz@users.noreply.github.com>
2023-06-22 20:28:41 +0200

    Przykładowy opis zadania 3

--- cpp.cpp
+++ cpp.cpp
@@ -16,3 +16,16 @@ int main()
     cout << "La\nto\n";
     return 0;
 }
+
+int main()
+{
+    int l=100;
+    cout << "l= " << l << endl;
+    cout << "2*l+1= " << 2*l+1 << endl;
+ 
+    cout << "Podaj liczbe: ";
+    cin >> l;
+    cout << "Wpisales " << l << endl;
+ 
+    return 0;
+}
--- java.java
+++ java.java
@@ -43,3 +43,16 @@ class Main {
     }
   }
 }
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TimeString {
+
+    public static void main(String[] args) {
+        // Format y-M-d or yyyy-MM-d
+        String string = "2017-07-25";
+        LocalDate date = LocalDate.parse(string, DateTimeFormatter.ISO_DATE);
+
+        System.out.println(date);
+    }
+}
